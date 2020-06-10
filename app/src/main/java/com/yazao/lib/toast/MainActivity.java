e3d326c82471d19a0096bdcaccfba4b6e1e244e1
zhaishaoping <zhaishaoping@yuelvhui.com>
2020-06-10 15:41:50 +0800

    调整demo中的appid

--- app/src/main/java/com/yazao/lib/toast/MainActivity.java
+++ app/src/main/java/com/yazao/lib/toast/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import com.yazao.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
